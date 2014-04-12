c6d76cf34db68c9a51b16017fce4c76ac013975c
Peter Felix Nguyen <peterfelixnguyen@gmail.com>
2014-04-11 19:37:00 -0700

    ZScore improved. Documentation added. Z score and P value now formatted to four decimal places.

--- src/pfnguyen/statlogic/zscore/ZScore.java
+++ src/pfnguyen/statlogic/zscore/ZScore.java
@@ -15,6 +15,8 @@
  */
 package pfnguyen.statlogic.zscore;
 
+import java.text.DecimalFormat;
+
 import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class ZScore {
@@ -22,21 +24,48 @@ public class ZScore {
     private double populationMean;
     private double stdDev;
     private double zScore;
+    private double probability;
     private NormalDistribution normal = new NormalDistribution();
 
+    /**
+     * Constructs ZScore based on parameter values.
+     * 
+     * @param  xBar            the sample mean
+     * @param  populationMean  the population mean
+     * @param  stdDev          the standard deviation
+     */
     public ZScore(double xBar, double populationMean, double stdDev) {
         this.xBar = xBar;
         this.populationMean = populationMean;
         this.stdDev = stdDev;
+        calculateZScore();
     }
 
-    public double getZScore() {
-        zScore = (xBar - populationMean) / stdDev;
+    /**
+     * Calculates the Z-score and its associated cumulative
+     * probability.
+     */
+    private void calculateZScore() {
+        DecimalFormat df = new DecimalFormat("#.####");
 
+        zScore = (xBar - populationMean) / stdDev;
+        zScore = Double.valueOf(df.format(zScore));
+
+        probability = normal.cumulativeProbability(zScore);
+        probability = Double.valueOf(df.format(probability));
+    }
+
+    /**
+     * @return  standardized Z-score
+     */
+    public double getZScore() {
         return zScore;
     }
 
+    /**
+     * @return  cumulative probability for Z-score
+     */
     public double getProbability() {
-        return normal.cumulativeProbability(zScore);
+        return probability;
     }
 }
