8a29ac8a7a7b8e1b0aa0b2dd5cfdff5b87769f9d
Peter Felix Nguyen <peterfelixnguyen@gmail.com>
2014-04-16 08:32:18 -0700

    Discarded commented code. Greatly improved the user friendliness of the calculator. Removed minor bugs in the way the calculator outputted. Added red background color for invalid inputs. Removed emptyFields (I recall that the code was useless or redundant). Output button now works. Refactored a variable that was improperly named (from xBar to rawScore).

--- src/pfnguyen/statlogic/gui/ZScorePanel.java
+++ src/pfnguyen/statlogic/gui/ZScorePanel.java
@@ -15,9 +15,10 @@
  */
 package pfnguyen.statlogic.gui;
 
-import java.awt.Dimension;
-//import java.awt.FlowLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -30,10 +31,15 @@ import javax.swing.event.DocumentListener;
 
 import pfnguyen.statlogic.zscore.ZScore;
 
+/**
+ * 
+ * 
+ * @author Peter "Felix" Nguyen
+ */
 @SuppressWarnings("serial")
 public class ZScorePanel extends JPanel {
-    private JLabel jlblXBar = new JLabel("X\u0305");
-    private JTextField jtfXBar = new JTextField(6);
+    private JLabel jlblRawScore = new JLabel("X");
+    private JTextField jtfRawScore = new JTextField(6);
     private JLabel jlblPopulationMean = new JLabel("\u03BC");
     private JTextField jtfPopulationMean = new JTextField(6);
     private JLabel jlblStdDev = new JLabel("\u03C3");
@@ -46,78 +52,84 @@ public class ZScorePanel extends JPanel {
     private FlowPanel row3 = new FlowPanel();
     private FlowPanel row4 = new FlowPanel();
     private FlowPanel row5 = new FlowPanel();
-    //private JPanel layoutContainer = new JPanel(new GridLayout(5, 1)); // This layout is for style
+    private FlowPanel row6 = new FlowPanel();
     // Values
-    private double xBar;
+    private double rawScore;
     private double populationMean;
     private double stdDev;
     private ZScore zScore;
+    // Boolean flags
+    private boolean isCalculated = false;
 
-    public ZScorePanel(final JTextArea jtaOutput, final JLabel statusBar) {
+    public ZScorePanel(final JTextArea jtaOutput, final JLabel statusBar, final StringBuilder outputString) {
         setBorder(new TitledBorder("Z-Score"));
-        //setLayout(new FlowLayout(FlowLayout.LEADING)); // This Layout is for style
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridLayout(6, 1));
         jlblZScore.setToolTipText(jlblZScore.getText());
         jlblPValue.setToolTipText(jlblPValue.getText());
-        //add(layoutContainer);
-        /*layoutContainer.*/add(row1);
-        /*layoutContainer.*/add(row2);
-        /*layoutContainer.*/add(row3);
-        /*layoutContainer.*/add(row4);
-        /*layoutContainer.*/add(row5);
-        row1.add(jlblXBar);
-        row1.add(jtfXBar);
-        row1.add(jlblPopulationMean);
-        row1.add(jtfPopulationMean);
-        row2.add(jlblStdDev);
-        row2.add(jtfStdDev);
-        row3.add(jlblZScore);
-        row4.add(jlblPValue);
-        row5.add(jbtOutput);
-        setMaximumSize(new Dimension(190, 185));
-        setMinimumSize(new Dimension(190, 185));
-        System.out.println(getPreferredSize()); // So I can adjust size accordinly
+        add(row1);
+        add(row2);
+        add(row3);
+        add(row4);
+        add(row5);
+        add(row6);
+        row1.add(jlblRawScore);
+        row1.add(jtfRawScore);
+        row2.add(jlblPopulationMean);
+        row2.add(jtfPopulationMean);
+        row3.add(jlblStdDev);
+        row3.add(jtfStdDev);
+        row4.add(jlblZScore);
+        row5.add(jlblPValue);
+        row6.add(jbtOutput);
 
-        jtfXBar.getDocument().addDocumentListener(new DocumentListener() {
+        jtfRawScore.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void changedUpdate(DocumentEvent e) {
-                try {
-                    xBar = new Double(jtfXBar.getText());
-                    System.out.println("(1) changed, new xBar value " + xBar);
-                    calculateZScore();
-                }
-                catch (NumberFormatException ex) {
-                    System.out.println("1 Not A Valid number");
-                    jlblZScore.setText(String.valueOf("Z = ?"));
-                    jlblPValue.setText(String.valueOf("P = ?"));
-                }
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
                 try {
-                    xBar = new Double(jtfXBar.getText());
-                    System.out.println("(2) removed, new xBar value " + xBar);
-                    calculateZScore();
+                    rawScore = new Double(jtfRawScore.getText());
+                    jtfRawScore.setBackground(Color.WHITE);
+                    try {
+                        populationMean = new Double(jtfPopulationMean.getText());
+                        stdDev = new Double(jtfStdDev.getText());
+                        calculateZScore();
+                    }
+                    catch (NumberFormatException ex) {
+                    }
                 }
                 catch (NumberFormatException ex) {
-                    System.out.println("1 Not A Valid number");
                     jlblZScore.setText(String.valueOf("Z = ?"));
                     jlblPValue.setText(String.valueOf("P = ?"));
+                    if (jtfRawScore.getText().length() != 0) {
+                        jtfRawScore.setBackground(Color.RED);
+                    }
+                    isCalculated = false;
                 }
             }
 
             @Override
             public void insertUpdate(DocumentEvent e) {
                 try {
-                    xBar = new Double(jtfXBar.getText());
-                    System.out.println("(3) inserted, new xBar value " + xBar);
-                    calculateZScore();
+                    rawScore = new Double(jtfRawScore.getText());
+                    jtfRawScore.setBackground(Color.WHITE);
+                    try {
+                        populationMean = new Double(jtfPopulationMean.getText());
+                        stdDev = new Double(jtfStdDev.getText());
+                        calculateZScore();
+                    }
+                    catch (NumberFormatException ex) {
+                    }
                 }
                 catch (NumberFormatException ex) {
-                    System.out.println("1 Not A Valid number");
                     jlblZScore.setText(String.valueOf("Z = ?"));
                     jlblPValue.setText(String.valueOf("P = ?"));
+                    if (jtfRawScore.getText().length() != 0) {
+                        jtfRawScore.setBackground(Color.RED);
+                    }
+                    isCalculated = false;
                 }
             }
         });
@@ -125,29 +137,28 @@ public class ZScorePanel extends JPanel {
         jtfPopulationMean.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void changedUpdate(DocumentEvent e) {
-                try {
-                    populationMean = new Double(jtfPopulationMean.getText());
-                    System.out.println("(1) changed, new populationMean value " + populationMean);
-                    calculateZScore();
-                }
-                catch (NumberFormatException ex) {
-                    System.out.println("2 Not A Valid number");
-                    jlblZScore.setText(String.valueOf("Z = ?"));
-                    jlblPValue.setText(String.valueOf("P = ?"));
-                }
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
                 try {
                     populationMean = new Double(jtfPopulationMean.getText());
-                    System.out.println("(2) removed, new populationMean value " + populationMean);
-                    calculateZScore();
+                    jtfPopulationMean.setBackground(Color.WHITE);
+                    try {
+                        rawScore = new Double(jtfRawScore.getText());
+                        stdDev = new Double(jtfStdDev.getText());
+                        calculateZScore();
+                    }
+                    catch (NumberFormatException ex) {
+                    }
                 }
                 catch (NumberFormatException ex) {
-                    System.out.println("2 Not A Valid number");
                     jlblZScore.setText(String.valueOf("Z = ?"));
                     jlblPValue.setText(String.valueOf("P = ?"));
+                    if (jtfPopulationMean.getText().length() != 0) {
+                        jtfPopulationMean.setBackground(Color.RED);
+                    }
+                    isCalculated = false;
                 }
             }
 
@@ -155,13 +166,22 @@ public class ZScorePanel extends JPanel {
             public void insertUpdate(DocumentEvent e) {
                 try {
                     populationMean = new Double(jtfPopulationMean.getText());
-                    System.out.println("(3) inserted, new populationMean value " + populationMean);
-                    calculateZScore();
+                    jtfPopulationMean.setBackground(Color.WHITE);
+                    try {
+                        rawScore = new Double(jtfRawScore.getText());
+                        stdDev = new Double(jtfStdDev.getText());
+                        calculateZScore();
+                    }
+                    catch (NumberFormatException ex) {
+                    }
                 }
                 catch (NumberFormatException ex) {
-                    System.out.println("2 Not A Valid number");
                     jlblZScore.setText(String.valueOf("Z = ?"));
                     jlblPValue.setText(String.valueOf("P = ?"));
+                    if (jtfPopulationMean.getText().length() != 0) {
+                        jtfPopulationMean.setBackground(Color.RED);
+                    }
+                    isCalculated = false;
                 }
             }
         });
@@ -169,29 +189,28 @@ public class ZScorePanel extends JPanel {
         jtfStdDev.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void changedUpdate(DocumentEvent e) {
-                try {
-                    stdDev = new Double(jtfStdDev.getText());
-                    System.out.println("(1) changed, new stdDev value " + stdDev);
-                    calculateZScore();
-                }
-                catch (NumberFormatException ex) {
-                    System.out.println("3 Not A Valid number");
-                    jlblZScore.setText(String.valueOf("Z = ?"));
-                    jlblPValue.setText(String.valueOf("P = ?"));
-                }
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
                 try {
                     stdDev = new Double(jtfStdDev.getText());
-                    System.out.println("(2) removed, new stdDev value " + stdDev);
-                    calculateZScore();
+                    jtfStdDev.setBackground(Color.WHITE);
+                    try {
+                        rawScore = new Double(jtfRawScore.getText());
+                        populationMean = new Double(jtfPopulationMean.getText());
+                        calculateZScore();
+                    }
+                    catch (NumberFormatException ex) {
+                    }
                 }
                 catch (NumberFormatException ex) {
-                    System.out.println("3 Not A Valid number");
                     jlblZScore.setText(String.valueOf("Z = ?"));
                     jlblPValue.setText(String.valueOf("P = ?"));
+                    if (jtfStdDev.getText().length() != 0) {
+                        jtfStdDev.setBackground(Color.RED);
+                    }
+                    isCalculated = false;
                 }
             }
 
@@ -199,37 +218,52 @@ public class ZScorePanel extends JPanel {
             public void insertUpdate(DocumentEvent e) {
                 try {
                     stdDev = new Double(jtfStdDev.getText());
-                    System.out.println("(3) inserted, new stdDev value " + stdDev);
-                    calculateZScore();
+                    jtfStdDev.setBackground(Color.WHITE);
+                    try {
+                        rawScore = new Double(jtfRawScore.getText());
+                        populationMean = new Double(jtfPopulationMean.getText());
+                        calculateZScore();
+                    }
+                    catch (NumberFormatException ex) {
+                    }
                 }
                 catch (NumberFormatException ex) {
-                    System.out.println("3 Not A Valid number");
                     jlblZScore.setText(String.valueOf("Z = ?"));
                     jlblPValue.setText(String.valueOf("P = ?"));
+                    if (jtfStdDev.getText().length() != 0) {
+                        jtfStdDev.setBackground(Color.RED);
+                    }
+                    isCalculated = false;
+                }
+            }
+        });
+
+        jbtOutput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isCalculated) {
+                    outputString.append("X = " + jtfRawScore.getText() + "\n" +
+                            "\u03BC = " + jtfPopulationMean.getText() + "\n" +
+                            "\u03C3 = " + jtfStdDev.getText() + "\n" +
+                            jlblZScore.getText() + "\n" +
+                            jlblPValue.getText() + "\n\n" +
+                            "**************************************************************************************" +
+                            "\n");
+                    jtaOutput.setText(outputString.toString());
                 }
             }
         });
     }
 
-    private boolean emptyFields() { // Not necessary
-        if (jtfXBar.getText().length() == 0 &&
-                jtfPopulationMean.getText().length() == 0 &&
-                jtfStdDev.getText().length() == 0)
-            return true;
-        else
-            return false;
-    }
-
+    /**
+     * Calculates and outputs the Z-Score and P-Value
+     */
     public void calculateZScore() {
-        if (new Double(jtfStdDev.getText()) != 0 && !emptyFields()) {
-            zScore = new ZScore(xBar, populationMean, stdDev);
+        if (new Double(jtfStdDev.getText()) != 0) { // condition NOT required for DOUBLES
+            zScore = new ZScore(rawScore, populationMean, stdDev);
             jlblZScore.setText("Z = " + String.valueOf(zScore.getZScore()));
             jlblPValue.setText("P = " + String.valueOf(zScore.getProbability()));
+            isCalculated = true;
         }
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(190, 185);
     }
 }
