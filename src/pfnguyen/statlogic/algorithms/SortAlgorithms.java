8c449d0d424975a159787724b29e5d28925a531c
Peter Felix Nguyen <peterfelixnguyen@gmail.com>
2014-04-16 07:58:38 -0700

    New class for a selection of sorting algorithms.

---
+++ src/pfnguyen/statlogic/algorithms/SortAlgorithms.java
@@ -0,0 +1,27 @@
+package pfnguyen.statlogic.algorithms;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class SortAlgorithms {
+
+    private SortAlgorithms() {
+    }
+
+    /**
+     * Inefficient sort for large lists
+     */
+    public static void BubbleSort(ArrayList<BigDecimal> values) {
+        for (int i = 0; i < values.size(); i++) {
+            for (int j = 0; j < values.size(); j++) {
+
+                if (j != values.size() - 1)
+                    if (values.get(j).compareTo(values.get(j + 1)) > 0) {
+                        BigDecimal temp = values.get(j);
+                        values.set(j, values.get(j + 1));
+                        values.set(j + 1, temp);
+                    }
+            }
+        }
+    }
+}
