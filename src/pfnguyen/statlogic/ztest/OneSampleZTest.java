79e91bc7439bcc744c0343b6bebacb83a441dafc
Peter Felix Nguyen <peterfelixnguyen@gmail.com>
2014-04-16 22:08:04 -0700

    Removed reminder/comment.

--- src/pfnguyen/statlogic/ztest/OneSampleZTest.java
+++ src/pfnguyen/statlogic/ztest/OneSampleZTest.java
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Work on OneSampleTTest.java first
- */
 package pfnguyen.statlogic.ztest;
 
 import java.math.BigDecimal;
